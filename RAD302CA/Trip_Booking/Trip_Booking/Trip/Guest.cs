1a01baa439a511543e7c777dc465d60debe92ac5
PaulShiels <pshielss@gmail.com>
2014-02-15 20:14:08 +0000

    Classes Added

---
+++ RAD302CA/Trip_Booking/Trip_Booking/Trip/Guest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trip_Booking.Controllers
+{
+    public class Guest
+    {
+        public string name { get; set; }
+
+        public Guest()
+        {
+
+        }
+
+        public Guest(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ RAD302CA/Trip_Booking/Trip_Booking/Trip/Trip.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trip_Booking.Controllers
+{
+    public class Trip
+    {
+        public int legs { get; set; }
+        public string name { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public int minimumGuests { get; set; }
+
+        public Trip()
+        {
+        }
+
+        public Trip(int legs, string name, DateTime startDate, DateTime endDate, int minimumGuests)
+        {
+            this.legs = legs;
+            this.name = name;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.minimumGuests = minimumGuests;
+        }
+    }
+}
